1a7b03e3f8917156ad070c55cf3f1ceb600d8875
suman <sumankumarghosh94@gmail.com>
2025-09-24 00:00:48 +0530

    add some

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import UserContext from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <UserContext>
+  <StrictMode>
     <App />
-  </UserContext>,
+  </StrictMode>,
 )
